146309972a6d54945a2e08487d85761b3e35a1d2
muhammad hadif nasarudin <hadif.nasarudin@gmail.com>
2025-04-28 14:20:27 +0800

    adding picture

---
+++ lib/view/loginScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Screen"),
+        backgroundColor: Colors.amber.shade600,  
+      ),
+    );
+  }
+}
--- lib/view/registerscreen.dart
+++ lib/view/registerscreen.dart
@@ -9,6 +9,12 @@ class Registerscreen extends StatefulWidget {
 
 class _RegisterscreenState extends State<Registerscreen> {
   TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController universityController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
@@ -26,12 +32,72 @@ class _RegisterscreenState extends State<Registerscreen> {
            padding: const EdgeInsets.all(8.0),
            child: Column(
             children: [
+              Container(
+                height: 200,
+                width: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/takePicture.png"),
+                    
+                  ),
+                ),
+              ) ,
               TextField(
                 controller: nameController,
                 decoration: const InputDecoration(
                   labelText: "Your name",
                 ),
-              )
+                keyboardType: TextInputType.text,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: "Your email",
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: "Your password",
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                controller: confirmPasswordController,
+                decoration: const InputDecoration(
+                  labelText: "Confirm password",
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  labelText: "Your phone",
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                controller: universityController,
+                decoration: const InputDecoration(
+                  labelText: "Your university",
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                controller: addressController,
+                decoration: const InputDecoration(
+                  labelText: "Your address",
+                ),
+                obscureText: true,
+              ),
+              TextButton(
+                child: const Text("Register"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber.shade900,
+                ),
+                onPressed: () {},
+              ),
             ],
            ), 
           ),
